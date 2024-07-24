3e316b3ff25be618619e57bb181a2ee88a3af4dd
krylovolegv <krylovolegv@yandex.ru>
2024-07-24 07:11:28 +0200

    test two

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def check(number):
-   return number % 2 == 0
+def is_palindrome(s):
+   return s == s[::-1]
+
+# def check(number):
+#    return number % 2 == 0
 
--- test.py
+++ test.py
@@ -1,18 +1,36 @@
 import pytest
-from main import check
+import pytest
+from main import is_palindrome  # замените my_module на имя вашего модуля
 
-def test_check():
-   assert check(6) == True
+def test_is_palindrome_true():
+   assert is_palindrome("madam") == True
 
-def test_check2():
-   assert check(3) == False
+def test_is_palindrome_false():
+   assert is_palindrome("hello") == False
 
-@pytest.mark.parametrize("number, expected", [
-   (2, True),
-   (5, False),
-   (0, True),
-   (56, True),
-   (-3, False)
+@pytest.mark.parametrize("s, expected", [
+   ("racecar", True),
+   ("python", False),
+   ("level", True),
+   ("", True),  # Пустая строка является палиндромом
 ])
-def test_check_with_param(number, expected):
-   assert check(number) == expected
+def test_is_palindrome_parametrized(s, expected):
+   assert is_palindrome(s) == expected
+
+# from main import check
+#
+# def test_check():
+#    assert check(6) == True
+#
+# def test_check2():
+#    assert check(3) == False
+#
+# @pytest.mark.parametrize("number, expected", [
+#    (2, True),
+#    (5, False),
+#    (0, True),
+#    (56, True),
+#    (-3, False)
+# ])
+# def test_check_with_param(number, expected):
+#    assert check(number) == expected
