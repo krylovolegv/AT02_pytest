bd84b8e178546e5d2f2f8886cc3c9721ab32438a
krylovolegv <krylovolegv@yandex.ru>
2024-07-24 07:14:07 +0200

    test three

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_palindrome(s):
-   return s == s[::-1]
+def sort_list(numbers):
+   return sorted(numbers)
+
+
+# def is_palindrome(s):
+#    return s == s[::-1]
 
 # def check(number):
 #    return number % 2 == 0
--- test.py
+++ test.py
@@ -1,21 +1,40 @@
 import pytest
-import pytest
-from main import is_palindrome  # замените my_module на имя вашего модуля
+from main import sort_list  # замените my_module на имя вашего модуля
 
-def test_is_palindrome_true():
-   assert is_palindrome("madam") == True
+def test_sort_list_ascending():
+   assert sort_list([3, 1, 2, 5, 4]) == [1, 2, 3, 4, 5]
 
-def test_is_palindrome_false():
-   assert is_palindrome("hello") == False
+def test_sort_list_descending():
+   assert sort_list([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
 
-@pytest.mark.parametrize("s, expected", [
-   ("racecar", True),
-   ("python", False),
-   ("level", True),
-   ("", True),  # Пустая строка является палиндромом
+def test_sort_list_mixed():
+   assert sort_list([-1, 3, 0, -2, 2]) == [-2, -1, 0, 2, 3]
+
+@pytest.mark.parametrize("numbers, expected", [
+   ([7, 2, 5, 3], [2, 3, 5, 7]),
+   ([10, -10, 0], [-10, 0, 10]),
+   ([], []),
+   ([1], [1])
 ])
-def test_is_palindrome_parametrized(s, expected):
-   assert is_palindrome(s) == expected
+def test_sort_list_parametrized(numbers, expected):
+   assert sort_list(numbers) == expected
+
+# from main import is_palindrome  # замените my_module на имя вашего модуля
+#
+# def test_is_palindrome_true():
+#    assert is_palindrome("madam") == True
+#
+# def test_is_palindrome_false():
+#    assert is_palindrome("hello") == False
+#
+# @pytest.mark.parametrize("s, expected", [
+#    ("racecar", True),
+#    ("python", False),
+#    ("level", True),
+#    ("", True),  # Пустая строка является палиндромом
+# ])
+# def test_is_palindrome_parametrized(s, expected):
+#    assert is_palindrome(s) == expected
 
 # from main import check
 #
