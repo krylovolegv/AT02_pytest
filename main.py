db931de5c8756aa1b6dcf8475b535b08753b58e2
krylovolegv <krylovolegv@yandex.ru>
2024-07-24 09:46:36 +0200

    test 3

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def sort_list(numbers):
-   return sorted(numbers)
+import sqlite3
 
+def init_db():
+   conn = sqlite3.connect(':memory:')
+   cursor = conn.cursor()
+   cursor.execute('''
+   CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY,
+   name TEXT,
+   age INTEGER)
+   ''')
+   return conn
+
+def add_user(conn, name, age):
+   cursor = conn.cursor()
+   cursor.execute('''
+   INSERT INTO users (name, age) VALUES (?, ?)''', (name, age))
+   conn.commit()
+
+def get_user(conn, name):
+   cursor = conn.cursor()
+   cursor.execute('''SELECT * FROM users WHERE name=?''', (name,))
+   return cursor.fetchone()
+
+# def sort_list(numbers):
+#    return sorted(numbers)
 
 # def is_palindrome(s):
 #    return s == s[::-1]
--- test.py
+++ test.py
@@ -1,23 +1,37 @@
 import pytest
-from main import sort_list  # замените my_module на имя вашего модуля
+from main import init_db, add_user, get_user
 
-def test_sort_list_ascending():
-   assert sort_list([3, 1, 2, 5, 4]) == [1, 2, 3, 4, 5]
 
-def test_sort_list_descending():
-   assert sort_list([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+@pytest.fixture
+def db_conn():
+   conn = init_db()
+   yield conn
+   conn.close()
 
-def test_sort_list_mixed():
-   assert sort_list([-1, 3, 0, -2, 2]) == [-2, -1, 0, 2, 3]
+def test_add_or_get_user(db_conn):
+   add_user(db_conn, "Sasha", 30)
+   user = get_user(db_conn, "Sasha")
+   assert user == (1, "Sasha", 30)
 
-@pytest.mark.parametrize("numbers, expected", [
-   ([7, 2, 5, 3], [2, 3, 5, 7]),
-   ([10, -10, 0], [-10, 0, 10]),
-   ([], []),
-   ([1], [1])
-])
-def test_sort_list_parametrized(numbers, expected):
-   assert sort_list(numbers) == expected
+# from main import sort_list  # замените my_module на имя вашего модуля
+#
+# def test_sort_list_ascending():
+#    assert sort_list([3, 1, 2, 5, 4]) == [1, 2, 3, 4, 5]
+#
+# def test_sort_list_descending():
+#    assert sort_list([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+#
+# def test_sort_list_mixed():
+#    assert sort_list([-1, 3, 0, -2, 2]) == [-2, -1, 0, 2, 3]
+#
+# @pytest.mark.parametrize("numbers, expected", [
+#    ([7, 2, 5, 3], [2, 3, 5, 7]),
+#    ([10, -10, 0], [-10, 0, 10]),
+#    ([], []),
+#    ([1], [1])
+# ])
+# def test_sort_list_parametrized(numbers, expected):
+#    assert sort_list(numbers) == expected
 
 # from main import is_palindrome  # замените my_module на имя вашего модуля
 #
